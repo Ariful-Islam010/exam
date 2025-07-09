ac92570e6697fedfe6493eea66f8a847dd312aad
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-07-09 12:42:50 +0600

    Update 2(a) length at most two.cpp

--- 2(a) length at most two.cpp
+++ 2(a) length at most two.cpp
@@ -1,1 +1,32 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    while(1)
+    {
+    cout<<"Enter the string : ";
+    getline(cin,s);
+    int i,count=0;
+    int len=s.length();
+
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='a'||s[i]=='b')
+            continue;
+        else
+        {
+           count=1;
+           break;
+        }
+    }
+    if(count==1)
+        cout<<"We put wrong alphabets"<<endl;
+    else if(len<=2)
+        cout<<"Accepted"<<endl;
+    else
+        cout<<"Rejected"<<endl;
+    }
+    return 0;
+}
